adbc35f93f926c529b81e55cffee756440ac33a2
radhay-samagra <radhay@samagragovernance.in>
2022-05-25 10:01:49 +0000

    Fix: #35 #34

--- src/components/Trainee/TraineeRegistration.js
+++ src/components/Trainee/TraineeRegistration.js
@@ -7,22 +7,25 @@ import { storeUser } from '../../common/globals';
 const TraineeRegistration = () => {
   let url = `${process.env.REACT_APP_GET_FORM}/getForm/traineeRegistration`;
   const customURL = localStorage.getItem('traineeRegistationURL');
+  localStorage.setItem('DST_USER_DATA', null);
   if (customURL !== null && customURL !== undefined && customURL !== 'undefined') url = customURL;
 
   useEffect(() => {
     window.addEventListener('message', (e) => {
       const { data } = e;
       try {
-        console.log('Debug');
         const decoded = JSON.parse(data);
         if (decoded.channel === 'traineeRegistration' && decoded.traineeId !== null) {
+          localStorage.setItem('DST_USER_DATA', null);
           setTimeout(() => {
             storeUser(response);
             localStorage.setItem('traineeId', decoded.traineeId);
             browserHistory.push('/trainee');
           }, 1000);
-        } else {
+        } else if (decoded.channel === 'traineeRegistration') {
           alert('Could not Register User');
+        } else {
+          console.log('Normal Flow');
         }
       } catch (error) {
         // console.log(e)
